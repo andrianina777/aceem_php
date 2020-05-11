c2142ce80c240a36a16c20537da8be22edbc0ec2
Anja <andrianina777@gmail.com>
2020-05-11 08:56:51 +0300

    maj

--- config/default.php
+++ config/default.php
@@ -1,4 +1,4 @@
 <?php
-// $base_url = 'http://localhost/php/projet_php_aceem';
-$base_url = 'http://localhost/aceem_php';
+$base_url = 'http://localhost/php/projet_php_aceem';
+//$base_url = 'http://localhost/aceem_php';
 $title = "Aceem";
