0192fa870731f0f47356dae9f29a097ac3fe365b
JO <mmjjonah@gmail.com>
2020-05-13 00:57:11 +0300

    eleves

--- pages/eleves/create.php
+++ pages/eleves/create.php
@@ -1,5 +1,5 @@
 <?php
-  include '../../controller/utilisateurs.php';
+  include '../../controller/eleves.php';
   require_once '../../layout/header.php'; 
   require_once '../../layout/sidebar.php';
   require_once '../../layout/navbar.php';
@@ -7,11 +7,11 @@
 <div class="content">
   <div class="container-fluid">
     <div class="container">
-      <form method="post">
-        <?= $data_user ? '<input type="text" name="id" value="'.$data_user->utilisateur_id.'" hidden="true">' :'' ?>
+      <form method="post" enctype="multipart/form-data">
+        <?= $data_eleve ? '<input type="text" name="id" value="'.$data_eleve->eleve_id.'" hidden="true">' :'' ?>
         <div class="row">
           <div class="col-md-2 flex content-space">
-            <button type="submit" name="submit_user" class="btn btn-danger"><?= $data_user ? 'Modifier' :'Enregister'?></button>&nbsp;  <span>ou</span>&nbsp;  
+            <button type="submit" name="submit_eleve" class="btn btn-danger"><?= $data_eleve ? 'Modifier' :'Enregister'?></button>&nbsp;  <span>ou</span>&nbsp;  
             <a href="./">Annuler</a>
           </div>
         </div>
@@ -24,7 +24,7 @@
                   <label for="nom">Nom:</label>
                 </div>
                 <div class="col-sm-9">
-                  <input type="text" name="nom" value="<?= $data_user ? $data_user->utilisateur_nom :'' ?>" id="nom" class="form-control form-control-sm " placeholder="Nom">
+                  <input type="text" name="nom" value="<?= $data_eleve ? $data_eleve->eleve_nom :'' ?>" id="nom" class="form-control form-control-sm " placeholder="Nom">
                 </div>
               </div>
             </div>
@@ -34,80 +34,132 @@
                   <label for="prenom">Prénom:</label>
                 </div>
                 <div class="col-sm-9">
-                  <input type="text" name="prenom" value="<?= $data_user ? $data_user->utilisateur_nom :'' ?>" id="prenom" class="form-control form-control-sm " placeholder="Prénom">
+                  <input type="text" name="prenom" value="<?= $data_eleve ? $data_eleve->eleve_prenom :'' ?>" id="prenom" class="form-control form-control-sm " placeholder="Prénom">
                 </div>
               </div>
             </div>
             <div class="col-sm-4">
               <div class="row">
                 <div class="col-sm-3">
+                  <label for="photo">Photo:</label>
+                </div>
+                <div class="col-sm-9">
+                  <div class="input-group">
+                    <input type="file" value="<?= $data_eleve ? $data_eleve->eleve_photo :'' ?>" class="form-control form-control-sm" id="photo" name="photo"><br>
+                  </div>
+                  <div class="text-danger"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row form-group">
+            <div class="col-sm-4">
+              <div class="row">
+                <div class="col-sm-3">
                   <label for="date_naissance">Date de naissance:</label>
                 </div>
-                <div class="col-sm-9 input-group">
-                    <div class="custom-file">
-                      <input type="file" class="custom-file-input">
-                      <label class="custom-file-label" for="customFile">Ajouter une image</label>
-                    </div>
-                  <!-- <div class="input-group">
+                <div class="col-sm-9">
+                  <div class="input-group">
                     <div class="input-group-prepend">
                       <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
                     </div>
-                    <input type="date" class="form-control form-control-sm" data-inputmask-alias="datetime" data-inputmask-inputformat="dd/mm/yyyy" data-mask="" im-insert="false">
-                  </div> -->
+                    <input type="date" name="date_naissance" id="date_naissance" value="<?= $data_eleve ? $data_eleve->eleve_date_naissance :'' ?>" class="form-control form-control-sm" data-inputmask-alias="datetime" data-inputmask-inputformat="dd/mm/yyyy" data-mask="" im-insert="false">
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="row">
+                <div class="col-sm-3">
+                  <label for="adresse">Adresse:</label>
+                </div>
+                <div class="col-sm-9">
+                  <input type="text" name="adresse" value="<?= $data_eleve ? $data_eleve->eleve_adresse :'' ?>" id="adresse" class="form-control form-control-sm " placeholder="Adresse">
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="row">
+                <div class="col-sm-3">
+                  <label for="matricule">Matricule:</label>
+                </div>
+                <div class="col-sm-3">
+                  <input type="text" name="matricule" value="<?= $data_eleve ? $data_eleve->eleve_matricule :'' ?>" id="matricule" class="form-control form-control-sm " placeholder="Matricule">
+                </div>
+                <div class="col-sm-3">
+                  <label for="numero">Numéro:</label>
+                </div>
+                <div class="col-sm-3">
+                  <input type="number" name="numero" value="<?= $data_eleve ? $data_eleve->eleve_numero :'' ?>" id="numero" class="form-control form-control-sm " placeholder="Numéro">
                 </div>
               </div>
             </div>
           </div>
           <div class="row form-group">
-            <div class="col-sm-6">
+            <div class="col-sm-4">
               <div class="row">
                 <div class="col-sm-3">
-                  <label for="pseudo">Pseudo : </label>
+                  <label for="date_inscription">Date d'inscription:</label>
                 </div>
                 <div class="col-sm-9">
-                  <input type="text" name="pseudo" value="<?= $data_user ? $data_user->utilisateur_pseudo :'' ?>" id="pseudo" class="form-control form-control-sm " placeholder="Pseudo">
+                  <div class="input-group">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
+                    </div>
+                    <input type="date" name="date_inscription" value="<?= $data_eleve ? $data_eleve->eleve_date_inscription :'' ?>" class="form-control form-control-sm" id="date_inscription" data-inputmask-alias="datetime" data-inputmask-inputformat="dd/mm/yyyy" data-mask="" im-insert="false">
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div class="row form-group">
-            <div class="col-sm-6">
+            <div class="col-sm-4">
               <div class="row">
                 <div class="col-sm-3">
-                  <label for="email">Email : </label>
+                  <label for="classe">Classe:</label>
                 </div>
-                <div class="col-sm-9">
-                  <input type="email" name="email" value="<?= $data_user ? $data_user->utilisateur_email :'' ?>" id="email" class="form-control form-control-sm " placeholder="ex@email.com">
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="row form-group">
-            <div class="col-sm-6">
-              <div class="row">
                 <div class="col-sm-3">
-                  <label for="email">Groupe : </label>
-                </div>
-                <div class="col-sm-9">
-                  <select name="groupe" class="form-control form-control-sm">
-                    <?php foreach($all_group as $key => $value): 
-                      $selected = $data_user->utilisateur_groupe_fk==$value['groupe_id'] ? 'selected' :'';
+                  <select id="classe" name="classe" class="form-control form-control-sm">
+                    <?php foreach ($all_classe as $i => $classe):
+                      $selected = isset($data_eleve) && $data_eleve->eleve_classe_param_fk==$classe['param_id'] ? 'selected' :'';
                     ?>
-                      <option value="<?=$value['groupe_id']?>" <?=$selected?>><?=$value['groupe_description']?></option>
+                      <option value="<?=$classe['param_id']?>" <?=$selected?>><?=$classe['param_description']?></option>
+                    <?php endforeach; ?>
+                  </select>
+                </div>
+                <div class="col-sm-3">
+                  <select name="classe_categorie" class="form-control form-control-sm">
+                    <option value="-1">Aucun</option>
+                    <?php foreach ($all_classe_cat as $i => $cat):
+                      $selected = isset($data_eleve) && $data_eleve->eleve_classe_cat_param_fk==$cat['param_id'] ? 'selected' :'';
+                    ?>
+                      <option value="<?=$cat['param_id']?>" <?=$selected?>><?=$cat['param_description']?></option>
+                    <?php endforeach; ?>
+                  </select>
+                </div>
+                <div class="col-sm-3">
+                  <select name="classe_mention" class="form-control form-control-sm">
+                    <option value="-1">Aucun</option>
+                    <?php foreach ($all_mention as $i => $mention):
+                      $selected = isset($data_eleve) && $data_eleve->eleve_classe_mention_param_fk==$mention['param_id'] ? 'selected' :'';
+                    ?>
+                      <option value="<?=$mention['param_id']?>" <?=$selected?>><?=$mention['param_description']?></option>
                     <?php endforeach; ?>
                   </select>
                 </div>
               </div>
             </div>
-          </div>
-          <div class="row form-group">
-            <div class="col-sm-6">
+            <div class="col-sm-4">
               <div class="row">
                 <div class="col-sm-3">
-                  <label for="password">Mot de passe : </label>
+                  <label for="session">Session:</label>
                 </div>
                 <div class="col-sm-9">
-                  <input type="password" name="password" id="password" class="form-control form-control-sm " placeholder="Mot de passe">
+                  <select name="session" id="session" class="form-control form-control-sm">
+                    <?php foreach ($all_session as $i => $session):
+                      $selected = isset($data_eleve) && $data_eleve->eleve_classe_session_param_fk==$session['param_id'] ? 'selected' :'';
+                    ?>
+                      <option value="<?=$session['param_id']?>" <?=$selected?>><?=$session['param_description']?></option>
+                    <?php endforeach; ?>
+                  </select>
                 </div>
               </div>
             </div>
--- pages/eleves/index.php
+++ pages/eleves/index.php
@@ -1,5 +1,5 @@
 <?php
-  require_once '../../controller/eleves.php';
+  include '../../controller/eleves.php';
   require_once '../../layout/header.php'; 
   require_once '../../layout/sidebar.php';
   require_once '../../layout/navbar.php';
@@ -14,61 +14,140 @@
               <a href="<?=$base_url?>/pages/eleves/create.php">
                 <button class="btn btn-danger">Créer</button>
               </a>
+              <div class="float-sm-right btn-pagination">
+                <span class="page-start">0</span> - <span class="page-end">0</span> / <span class="page-total">0</span>&nbsp; 
+                <a href="javascript:void(0)" class="fas fa-arrow-alt-circle-left" onclick="pagePrecedent()"></a>
+                <a href="javascript:void(0)" class="fas fa-arrow-alt-circle-right" onclick="pageSuivant()"></a>
+              </div>
             </div>
           </div>
           <div class="row form-group">
             <div class="col-md-1"></div>
             <div class="col-md-2">
-              <label for="nom_search">Nom :</label>
+              <label for="description">Recherche :</label>
             </div>
             <div class="col-md-5">
-              <input type="text" id="nom_search" class="form-control">
-            </div>
-          </div>
-          <div class="row form-group">
-            <div class="col-md-1"></div>
-            <div class="col-md-2">
-              <label for="matricule_search">Matricule :</label>
-            </div>
-            <div class="col-md-5">
-              <input type="text" id="matricule_search" class="form-control">
+              <input type="text" id="search" class="form-control">
             </div>
           </div>
         </div>
       </div>
-      <main>
         
-        <div class="card-body">
-          <table id="liste_eleve" class="table table-bordered table-hover">
+      <main class="row">
+        <div class="col-sm-12">
+          <table id="JodataTable" class="table table-bordered table-hover">
             <thead>
-            <tr>
-              <th>Name</th>
-                <th>Position</th>
-                <th>Office</th>
-                <th>Extn.</th>
-                <th>Start date</th>
-                <th>Salary</th>
-            </tr>
+              <tr>
+                <th>Photo</th>
+                <th>Matricule</th>
+                <th>Nom et prénom</th>
+                <th>Classe</th>
+                <th>Session</th>
+                <th>Date d'incription</th>
+                <th>Action</th>
+              </tr>
             </thead>
           </table>
         </div>
       </main>
+      <div class="clearfix"></div>
+      <select class="form-control col-sm-2" id="JoPaginate">
+        <option value="10" selected>10 affichés</option>
+        <option value="20">20 affichés</option>
+        <option value="50">50 affichés</option>
+      </select>
     </div>
   </div>
 <script>
-  console.log($('#liste_eleve'));
+
+  table = null;
+
   $(document).ready(function() {
-    $('#liste_eleve').DataTable( {
-        "ajax": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/730692/json.txt",
+    init_table();
+  } );
+
+  $('#JoPaginate').change(() => {
+    $('#JodataTable').DataTable().page.len($('#JoPaginate').val()).draw();
+  })
+
+  $('#search').on( 'keyup', function () {
+    table.search( this.value ).draw();
+    init_page_info()
+  } );
+
+  function init_page_info() {
+    table.on('draw', () => {
+      let tInfo = table.page.info();
+      $('.page-start').html(tInfo.start + 1) 
+      $('.page-end').html(tInfo.end) 
+      $('.page-total').html(tInfo.recordsDisplay) 
+      $('body .odd .dataTables_empty').html('Aucun résultat trouvé')
+    })
+  }
+
+  function init_table(url='<?= $base_url ?>/controller/eleves.php?list=0') {
+    table = $('#JodataTable').DataTable( {
+        "ajax": url,
         "columns": [
-            { "data": "name" },
-            { "data": "position" },
-            { "data": "office" },
-            { "data": "extn" },
-            { "data": "start_date" },
-            { "data": "salary" }
+            { "data": (data, type, full) => {
+              return `<img src="<?=$base_url?>/resources/eleves/${data.eleve_photo}" height="80px">`;
+            } },
+            { "data": "eleve_matricule" },
+            { "data": (data, type, full) => {
+              return `${data.eleve_nom} ${data.eleve_prenom}`;
+            } },
+            { "data": (data, type, full) => {
+              return `${data.classe} ${data.classe_cat!=null?data.classe_cat:''} ${data.mention!=null?data.mention:''}`;
+            } },
+            { "data": "session" },
+            { "data": "eleve_date_inscription" },
+            {
+              "data": function(data, type, full) {
+                  let btn = `
+                  <div class="flex content-space">
+                    <a href="<?=$base_url?>/pages/eleves/create.php?id=${data.eleve_id}"><i class="fa fa-edit"></i></a>
+                    <a href="javascript:void(0)" onclick="delete_eleve(${data.eleve_id})"><i class="fa fa-trash text-danger"></i></a>
+                  </div>
+                  `;
+                  return btn;
+                }
+            }
         ]
     } );
-  } );
+    init_page_info()
+  }
+
+  function delete_eleve(eleve_id) {
+    Swal.fire({
+      title: 'Êtes-vous sûre de le supprimer?',
+      text: `L'action est irreversible.`,
+      confirmButtonText: 'Supprimer',
+      cancelButtonText: 'Annuler',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+    }).then((result) => {
+      if (result.value) {
+        $.ajax({
+          url: '<?=$base_url?>/controller/eleves.php?delete='+ eleve_id,
+          type: 'post',
+          success: (r) => {
+            if (r.status === 'success') {
+              table.destroy();
+              init_table()
+              Swal.fire('Supprimer', 'Élement supprimer avec succès.', 'success')
+            } else {
+              Swal.fire('Erreur', `Une erreur s'est produit, veuillez réessayer!`, 'error');
+            }
+          },
+          error: (error) => {
+            console.log(error)
+            Swal.fire('Erreur', `Une erreur s'est produit, veuillez réessayer!`, 'error');
+          }
+        })
+      }
+    })
+  }
 </script>
 <?php require_once '../../layout/footer.php'; ?>
